68097196608dba5f0ec1ea5e007f3000f0c68c43
xiaoyaoyimei <1206902591@qq.com>
2017-08-23 14:06:59 +0800

    five

--- js/public.js
+++ js/public.js
@@ -6,4 +6,11 @@ $(function(){
 			e.preventDefault();
 			window.location.href="index_4.html";
 			})
+			$('.title-list .title-tab img').click(function(e){
+				var index=$(this).data('index');
+				var title=$('.title-tab');
+				var cont=$('.cont-wrap');
+				title.eq(index).siblings().hide().end().show();
+				cont.eq(index).show().siblings('.cont-wrap').hide();
+				})
 	})
